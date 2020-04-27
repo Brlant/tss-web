944612049cc417ba4567bf7408847ca4891b9f25
Hulk <jayzch@qq.com>
2020-04-27 23:38:06 +0800

    日志

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     https: false,
     hotOnly: false, disableHostCheck: true, proxy: {
       '/api': {
-        target: 'https://dev-tss.cdcerp.cn',
+        target: 'https://sh-tss.cdcerp.cn',
         changOrigin: true
       }
     }
